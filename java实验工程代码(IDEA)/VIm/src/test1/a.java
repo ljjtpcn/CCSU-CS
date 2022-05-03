b6a847daa62d4593049793b2b357c1a214de261c
ljjtpcn <ljjtpcn@163.com>
2022-05-03 11:47:59 +0800

    update

--- java实验工程代码(IDEA)/VIm/src/test1/a.java
+++ java实验工程代码(IDEA)/VIm/src/test1/a.java
@@ -4,11 +4,9 @@ import java.io.*;
 import java.util.Scanner;
 
 public class a {
-    public static void main(String[] args) throws IOException {
-        Scanner in =new Scanner(System.in);
-        int age=in.nextInt();
-        stu num=new stu();
-        num.setting(age);
-        System.out.println(num.getting());
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("file.separator"));
     }
 }
