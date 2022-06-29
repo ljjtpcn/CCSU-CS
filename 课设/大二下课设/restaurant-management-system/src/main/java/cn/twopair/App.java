82ab22c463c48b062c55f636b3fa8ed6bfc23532
ljjtpcn <ljjtpcn@163.com>
2022-06-29 14:43:43 +0800

    update

---
+++ 课设/大二下课设/restaurant-management-system/src/main/java/cn/twopair/App.java
@@ -0,0 +1,12 @@
+package cn.twopair;
+
+import cn.twopair.view.LoginView;
+
+/**
+ * @author ljjtpcn
+ */
+public class App {
+    public static void main(String[] args) {
+        new LoginView();
+    }
+}
